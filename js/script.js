38bc0bccd45296cc441ae88da9ec549d67e8be0f
Lucas Araujo <lucasao744@gmail.com>
2023-12-13 16:57:42 -0300

    Fazendo a comida

--- js/script.js
+++ js/script.js
@@ -1,20 +1,52 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
+//Tem que arrumar o audio
+//const audio = new audio("../assets/audio.mp3");
+
 const size = 30;
 
 const snake = [
     {x: 270, y: 240},
 ];
 
+const randomNumber = (max, min)=>{
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+const randomPosition = (max, min)=>{
+    const number = randomNumber(0, canvas.width - size);
+    return  Math.round(number / 30) * 30;
+}
+
+const randomColor = ()=>{
+    const red = randomNumber(0, 255);
+    const green = randomNumber(0, 255);
+    const blue = randomNumber(0, 255);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+const food = {
+    x: randomPosition(),
+    y:randomPosition(),
+    color: randomColor()
+};
 let direction, loopId;
 
+const drowFood = ()=>{
+    const {x, y, color} = food;
+    ctx.shadowColor = color;
+    ctx.shadowBlur = 20;
+    ctx.fillStyle = color;
+    ctx.fillRect(food.x, food.y, size, size);
+    ctx.shadowBlur = 0;
+}
+
 const drawSnake = ()=>{
-    ctx.fillStyle = "#ddd"
+    ctx.fillStyle = "white"
     
     snake.forEach((position, index)=>{
         if(index == snake.length - 1){
-            ctx.fillStyle = "white"
+            ctx.fillStyle = "gray"
         }
         ctx.fillRect(position.x, position.y, size, size);
     });
@@ -58,18 +90,40 @@ const drawGrid = ()=>{
     }
 }
 
+const  chackEat = ()=>{
+    const head = snake[snake.length - 1];
+
+    if (head.x == food.x && head.y == food.y){
+        snake.push(head);
+        //audio.play();
+
+        let x = randomPosition();
+        let y = randomPosition();
+
+        while (snake.find((position)=> position.x == x && position.y == y)){
+            x = randomPosition();
+            y = randomPosition();
+        }
+        food.x = x;
+        food.y = y;
+        food.color = randomColor();
+    }
+
+}
 
 const gameLoop = ()=>{
     clearInterval(loopId);
 
     ctx.clearRect(0, 0, 600, 600);
     drawGrid();
+    drowFood();
     moveSnake();
     drawSnake();
+    chackEat();
 
    loopId = setTimeout(()=>{
         gameLoop();
-    }, 300);
+    }, 200);
 };
 
 gameLoop();
